88b50a35207e088a63e893c239696654c1bbd99f
GradyJean <grady.jean.king@gmail.com>
2025-12-09 17:10:07 +0800

    fix(collector): 修复文件路径变更时偏移量重置逻辑
    - 添加文件路径变更时的日志记录
    - 修复偏移量重置逻辑，确保在文件路径变化时正确重置
    - 更新当前文件路径并记录日志
    - 在元数据保存后正确更新偏移量
    - 优化日志信息，提高调试可追踪性

--- core/scheduler/tasks/collector_task.py
+++ core/scheduler/tasks/collector_task.py
@@ -40,9 +40,11 @@ class LogCollectorTask(TaskRunner):
         if file_path != self.current_file_path:
             logger.info(f"file path change to: {file_path}")
             file_path = self.current_file_path
+            logger.info(f"reset file path: {file_path}")
             self.file_path_changed = True
+            logger.info(f"set file path change status:{self.file_path_changed}")
             self.current_file_path = settings.nginx.get_log_path()
-
+            logger.info(f"current file path update: {self.current_file_path}")
         logger.info(f"current collecting file path: {file_path}:[{offset}]:[{self.log_metadata_service.index}]")
         self.collector.start(file_path=file_path, offset=offset)
 
@@ -56,11 +58,14 @@ class LogCollectorTask(TaskRunner):
         if not metadata_list:
             return True
         save_status = self.log_metadata_service.batch_insert(metadata_list)
+        logger.info(f"save log meta data: {len(metadata_list)} status: {save_status} current offset:{offset}")
         if self.file_path_changed:
             # 文件改变时 重置偏移量
             offset = 0
             self.file_path_changed = False
+            logger.info(f"file path change, reset offset, set file path change status:{self.file_path_changed}")
         if save_status:
+            logger.info(f"offset update: {offset}")
             return self.offset_service.save_offset(offset)
         return False
 
